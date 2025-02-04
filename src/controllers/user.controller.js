bb2aeaef31fcf942ed513d076b3a79ab18c5d65b
Harshit <harshit2001pandey@gmail.com>
2025-02-04 12:19:53 +0530

    Route Setup

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,3 +3,11 @@ import express from 'express';
 export const signUp = (req, res) => {
     res.status(201).send("Sign up route");
 }
+
+export const login = (req, res) => {
+    res.status(200).send("Sign in route");
+}
+
+export const logout = (req, res) => {
+    res.status(200).send("Logout route");
+}
--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,10 +1,12 @@
 import express from 'express';
-import { signUp } from '../controllers/user.controller.js';
+import { logout, login, signUp } from '../controllers/user.controller.js';
 
 
 const router = express.Router();
 
 router.post('/signup', signUp);
+router.post('/login', login);
+router.post('/logout', logout);
 
 
 export default router;
