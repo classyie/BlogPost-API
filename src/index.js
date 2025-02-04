fb1b7be1611faa5d22dd2e71d3cb81e107a336ae
Harshit <harshit2001pandey@gmail.com>
2025-02-04 12:28:31 +0530

    DB connection setup

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import userAuth from './routes/user.route.js'
+import connectDB from './lib/db.js';
+
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5001;
@@ -9,4 +11,5 @@ app.use('/auth', userAuth);
 
 app.listen(PORT, ()=>{
     console.log(`Server is running on port ${PORT}`);
+    connectDB();
 });
