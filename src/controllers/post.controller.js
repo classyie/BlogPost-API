2ffc06c539f10fda0ce3c88679d9d6be336a7933
Harshit <harshit2001pandey@gmail.com>
2025-02-06 09:56:40 +0530

    Update Post And Delete

--- src/controllers/post.controller.js
+++ src/controllers/post.controller.js
@@ -1,5 +1,6 @@
 import Post from "../models/post.model.js";
 import User from "../models/users.model.js";
+import mongoose from "mongoose";
 
 export const getPosts = async (req, res) => {
   try {
@@ -12,18 +13,73 @@ export const getPosts = async (req, res) => {
 };
 
 export const createPost = async (req, res) => {
-    const {title, content} = req.body;
-    const user = req.user;
-    if(!title || !content) {
-        return res.status(400).send({ message: "Title and content are required." });
-    }
-    try{
-        const newPost = new Post({ title, content, author: user._id });
-        await newPost.save();
-        res.status(201).send({message: "Post created successfully", post: newPost});
-    }
-    catch(error){
-        console.error("Error creating post:", error.message);
-        res.status(500).send({ success: false, message: "Internal Server Error" });
-    }
+  const { title, content } = req.body;
+  const user = req.user;
+  if (!title || !content) {
+    return res.status(400).send({ message: "Title and content are required." });
+  }
+  try {
+    const newPost = new Post({ title, content, author: user._id });
+    await newPost.save();
+    res.status(201).send({ message: "Post created successfully", post: newPost });
+  }
+  catch (error) {
+    console.error("Error creating post:", error.message);
+    res.status(500).send({ success: false, message: "Internal Server Error" });
+  }
 };
+
+export const updatePost = async (req, res) => {
+  const id = req.params.id;  // Use req.params.id directly
+  const { title, content } = req.body;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).send({ message: "Invalid Post ID." });
+  }
+
+  if (!title && !content) {
+    return res.status(400).send({ message: "Title or content is required." });
+  }
+
+  const user = req.user;
+
+  try {
+    const post = await Post.findById(id);
+    if (!post) {
+      return res.status(404).send({ message: "Post not found." });
+    }
+    if (post.author.toString() !== user._id.toString()) {
+      return res.status(403).send({ message: "You are not authorized to update this post." });
+    }
+
+    const updatedPost = await Post.findByIdAndUpdate(
+      id,
+      { $set: { ...(title && { title }), ...(content && { content }) } },
+      { new: true }
+    );
+
+    res.status(200).send({ message: "Post updated successfully", post: updatedPost });
+  } catch (error) {
+    console.error("Error updating post:", error.message);
+    res.status(500).send({ success: false, message: "Internal Server Error" });
+  }
+};
+
+
+export const deletePost = async (req, res) => {
+  const id = req.params.id;
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).send({ message: "Invalid Post ID." });
+  }
+  const user = req.user;
+  const post = await Post.findById(id);
+
+  if (!post) {
+    return res.status(404).send({ message: "Post not found." });
+  }
+  if (post.author.toString()!== user._id.toString()) {
+    return res.status(403).send({ message: "You are not authorized to delete this post." });
+  }
+  const deletedPost = await Post.deleteOne(post);
+  res.status(200).send({ message: "Post deleted successfully", post: deletedPost });
+}
--- src/routes/post.route.js
+++ src/routes/post.route.js
@@ -1,9 +1,11 @@
 
 import express from 'express';
-import { createPost, getPosts } from '../controllers/post.controller.js';
+import { createPost, deletePost, getPosts, updatePost } from '../controllers/post.controller.js';
 import { authMiddleware } from "../middleware/auth.middleware.js"
 const route = express.Router();
 
 route.get('/', getPosts);
 route.post('/create', authMiddleware, createPost);
+route.put('/update-post/:id', authMiddleware, updatePost);
+route.delete('/delete-post/:id', authMiddleware, deletePost);
 export default route;
